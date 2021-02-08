99c662e5c340484fb471f7b801d5859f7752447a
RhiannonAW123 <rhiannonaimeewilson@gmail.com>
2021-02-08 16:58:13 +0000

    first commit

---
+++ src/test/java/WaterBottleTest.java
@@ -0,0 +1,17 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class WaterBottleTest {
+
+    WaterBottle waterBottle;
+
+    @Before
+    public void before(){ waterBottle = new WaterBottle()}
+
+    @Test
+    public void volumeStartsAt100(){
+        assertEquals(100, waterBottle.getVolume());
+    }
+}
